493da6946385e2b59a404d36923fc245a0b24bba
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-12-22 12:52:56 +0000

    creating pms

--- frontend/scripts/UI/Panel.js
+++ frontend/scripts/UI/Panel.js
@@ -2,16 +2,26 @@ var Panel = window['Panel'] = function(params){
 	var self = this;
 	var className = params[S.CLASS_NAME];
 	var classNames = params[S.CLASS_NAMES];
+	var propertyNameVisible = params[S.PROPERTY_NAME_VISIBLE];
+	var model = params[S.MODEL];
 	var element = E.DIV();
 	element.classList.add('panel');
 	
+	var propertyBindingVisible;
+	if(propertyNameVisible){
+		propertyBindingVisible = PropertyBinding[S.STANDARD](this, model, propertyNameVisible, visibleChanged);
+	}
 	if (className)
 		element.classList.add(className);
 	if (classNames)
 		each(classNames, function (className) {
 			element.classList.add(className);
 		});
-	this['getElement']=function(){
+	this[S.GET_ELEMENT]=function(){
 		return element;
 	};
+	function visibleChanged(value){
+		if(value)element.classList.add('visible');
+		else element.classList.remove('visible');
+	}
 };
